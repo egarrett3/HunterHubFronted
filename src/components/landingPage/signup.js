ed5f2defa0d46933b85f07a9eebc746da6c99837
edwardg <egarrett3@fordham.edu>
2021-06-02 15:46:43 -0600

    add btn class & update form autocomplete

--- src/components/landingPage/signup.js
+++ src/components/landingPage/signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { signup } from "../../util/authentication";
 
-const Signup = ({updateValue,username,password,email}) => {
+const Signup = ({updateValue,username,password,email,toggle}) => {
 
    const handleSubmit = (e) => {
      e.preventDefault();
@@ -17,38 +17,48 @@ const Signup = ({updateValue,username,password,email}) => {
 
    return (
      <>
-        <form onSubmit={handleSubmit}>
-            <input
-                className="signup-input"
-                type="text"
-                name="signup-username"
-                value={username}
-                onChange={updateValue}
-                placeholder="username"
-            ></input>
-            <input
-                className="signup-input"
-                type="email"
-                name="signup-email"
-                value={email}
-                onChange={updateValue}
-                placeholder="email"
-            ></input>
-            <input
-                className="signup-input"
-                type="text"
-                name="signup-password"
-                value={password}
-                onChange={updateValue}
-                placeholder="password"
-            ></input>
-            <input
-                className="submitLoginForm"
-                type="submit"
-                value="Sign Up"
-            ></input>
-        </form>
-    
+       <form 
+        className="user-auth-form" 
+        onSubmit={handleSubmit}
+        autoComplete="off"
+       >
+         <input
+          autoComplete='false'
+          className="signup-username"
+          type="text"
+          name="signup-username"
+          value={username}
+          onChange={updateValue}
+          placeholder="username"
+         ></input>
+         <input
+          autoComplete='false'
+          className="signup-email"
+          type="email"
+          name="signup-email"
+          value={email}
+          onChange={updateValue}
+          placeholder="email"
+         ></input>
+         <input
+          className="signup-password"
+          type="text"
+          name="signup-password"
+          value={password}
+          onChange={updateValue}
+          placeholder="password"
+         ></input>
+         <input
+          className="signup-btn btn"
+          type="submit"
+          value="Sign Up"
+         ></input>
+       </form>
+       <span className='form-text'>
+         {" "}
+         Already have an account? 
+         <span className="switch-form" onClick={toggle}> Login </span>{" "}
+       </span>
      </>
    );
 }
