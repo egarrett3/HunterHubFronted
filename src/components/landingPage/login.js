bc07d547c2f735d772e53e15743b76dd160764d6
edwardg <egarrett3@fordham.edu>
2021-06-02 15:47:09 -0600

    add btn class & update form autocomplete

--- src/components/landingPage/login.js
+++ src/components/landingPage/login.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { login } from "../../util/authentication";
 
-const Login = ({updateValue,username,password,email}) => {
+const Login = ({updateValue,username,password,email,toggle}) => {
 
    const handleSubmit = (e) => {
      e.preventDefault();
 
      const user = {
-       username: username,
        email: email,
        password: password,
      };
@@ -17,37 +16,50 @@ const Login = ({updateValue,username,password,email}) => {
 
    return (
      <>
-        <form onSubmit={handleSubmit}>
-            <input
-                className="login-input"
-                type="text"
-                name="login-username"
-                value={username}
-                onChange={updateValue}
-                placeholder="username"
-            ></input>
-            <input
-                className="login-input"
-                type="email"
-                name="login-email"
-                value={email}
-                onChange={updateValue}
-                placeholder="email"
-            ></input>
-            <input
-                className="login-input"
-                type="text"
-                name="login-password"
-                value={password}
-                onChange={updateValue}
-                placeholder="password"
-            ></input>
-            <input
-                className="submitLoginForm"
-                type="submit"
-                value="Login"
-            ></input>
-        </form>
+       <form 
+        className="user-auth-form"
+        onSubmit={handleSubmit} 
+        autoComplete="off"
+       >
+         {/* <input
+           className="login-input"
+           type="text"
+           name="login-username"
+           value={username}
+           onChange={updateValue}
+           placeholder="username"
+         ></input> */}
+         <input
+          autoComplete='false'
+          className="login-email"
+          type="text"
+          name="login-email"
+          value={email}
+          onChange={updateValue}
+          placeholder="email"
+         ></input>
+         <input
+          className="login-password"
+          type="text"
+          name="login-password"
+          value={password}
+          onChange={updateValue}
+          placeholder="password"
+         ></input>
+         <input 
+          className="login-btn btn" 
+          type="submit" 
+          value="Login"
+         ></input>
+       </form>
+       <span className='form-text'>
+         {" "}
+         Don&apos;t have an account? 
+         <span className="switch-form" onClick={toggle}>
+           {" "}
+           Sign Up{" "}
+         </span>{" "}
+       </span>
      </>
    );
 }
