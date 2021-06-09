093f05c9d7bfa122b9fff55057b52229d34aac38
edwardg <egarrett3@fordham.edu>
2021-06-08 23:27:57 -0600

    add validation Component

---
+++ src/components/landingPage/validations.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { clearErrors } from '../../actions/auth_actions'; 
+import isEmpty from 'lodash/isEmpty';
+
+const Validation = ({children,attachErrorMessage,isLoginSelected,authErrors}) => {
+
+    const dispatch = useDispatch();
+
+    // const authErrors = useSelector((state) => {
+    //     return state.errors
+    // });
+
+    function filterErrors(authErrors) {
+        const keys = Object.keys(children);
+        const userAuthReqs = document.querySelector('.userAuth-reqs');
+
+        const userAuthReqMessages = keys.map((key) => {
+            if (!children[key]) {
+                return attachErrorMessage(key);
+            }
+        }).join("");
+
+    
+        const authErrorArray = authErrors ? Object.values(authErrors) : undefined;
+        const serverAuthErrors = authErrorArray.map(error => {
+            return `<li>${error}</li>`; 
+        }).join("")
+        
+        userAuthReqs.innerHTML = userAuthReqMessages || serverAuthErrors;
+    }
+
+    useEffect(() => {
+        filterErrors(authErrors);
+
+        if (isLoginSelected && !isEmpty(authErrors)) {
+            dispatch(clearErrors())
+        }
+    }, [children, authErrors, isLoginSelected]);
+
+    return (
+        <>
+            <ul className="userAuth-reqs"></ul>
+        </>
+    );
+}
+
+export default Validation;
