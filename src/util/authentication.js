844b12146adb0d5ea2c18722d5737ce28447e581
edwardg <egarrett3@fordham.edu>
2021-06-08 23:29:13 -0600

    remove .then promise and validation GET req

--- src/util/authentication.js
+++ src/util/authentication.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
 
 export const login = (user) => {
-    axios({
+    return axios({
         method: 'POST',
         url:'/api/users/login',
         data: user
-    })
-      .then(response => console.log(response))
-      .catch(err => console.log(err))
+    });
 }
 
 export const signup = (user) => {
-    axios({
+    return axios({
       method: "POST",
       url: "/api/users/register",
       data: user,
-    })
-      .then((response) => console.log(response))
-      .catch((err) => console.log(err));
+    });
 }
+
+// export const validatePassword = (password) => {
+//     return axios({
+//         method: "GET",
+//         url: '/api/users/validate',
+//         data: password
+//     });
+// }
