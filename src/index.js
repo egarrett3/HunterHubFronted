f380a706e048319eb58bcb00f3823b9a044d5587
edwardg <egarrett3@fordham.edu>
2021-06-08 23:37:51 -0600

    add store to Root component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root'
+import configureStore from './store/store';
 import reportWebVitals from './reportWebVitals';
 import 'normalize.css';
 
 window.addEventListener('DOMContentLoaded', () => {
+  let store = {};
+  let preloadedState = {}
+  store = configureStore(preloadedState);
 
   const root = document.getElementById("root");
-  ReactDOM.render(<Root />, root)
+  ReactDOM.render(<Root store={store}/>, root)
 })
 
 // If you want to start measuring performance in your app, pass a function
