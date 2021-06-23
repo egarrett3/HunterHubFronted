dbc0bfbba24e4d48cba20a6e55ea465f6af7cc35
edwardg <egarrett3@fordham.edu>
2021-06-23 16:04:13 -0600

    add redux persist to configurestore

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const configureStore = (preloadedState = {}) => {
 
-    return createStore(
-        rootReducer,
+    let store = createStore(
+        persistedReducer,
         preloadedState,
         applyMiddleware(thunk,logger),
     )
+    let persistor = persistStore(store);
+    return {store,persistor};
 }
 
 export default configureStore;
