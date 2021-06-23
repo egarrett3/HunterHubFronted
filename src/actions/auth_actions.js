0f9736b5299d53060442499785a76221aae15f79
edwardg <egarrett3@fordham.edu>
2021-06-23 16:02:17 -0600

    rename action and action-type

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,11 +1,12 @@
 import * as APIutil from '../util/authentication';
 
-export const LOGIN_CURRENT_USER = "LOGIN_CURRENT_USER";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_AUTH_ERRORS = "RECEIVE_AUTH_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const LOGIN_ROUTE = "LOGIN_ROUTE";
 
-export const loginCurrentUser = (user) => ({
-    type: LOGIN_CURRENT_USER,
+export const receiveCurrentUser = (user) => ({
+    type: RECEIVE_CURRENT_USER,
     user
 })
 
@@ -18,16 +19,15 @@ export const clearErrors = () => ({
     type: CLEAR_ERRORS
 })
 
-
 export const login = (user) => dispatch => {
     APIutil.login(user)
-        .then(user => dispatch(loginCurrentUser(user)))
+        .then(user => dispatch(receiveCurrentUser(user)))
             .catch(err => dispatch(receiveErrors(err.response.data)))
 }
 
 export const signup = (user) => dispatch => {
     APIutil.signup(user)
-        .then(user => dispatch(loginCurrentUser(user)))
+        .then(user => dispatch(receiveCurrentUser(user)))
             .catch(err => dispatch(receiveErrors(err.response.data)))
 }
 
