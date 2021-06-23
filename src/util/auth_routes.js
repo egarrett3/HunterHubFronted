1188d9dffd172c30773a2f8870b5d8a75c7de726
edwardg <egarrett3@fordham.edu>
2021-06-23 16:00:43 -0600

    added Protected route

---
+++ src/util/auth_routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Redirect, Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Private = ({component: Component, isAuthenticated, path, exact}) => {
+  return (<Route  
+    path={path}
+    exact={exact}
+    render={props => 
+      isAuthenticated ? <Component {...props} /> : <Redirect to='/' /> }
+  />)
+}
+
+const Protected = ({component: Component, isAuthenticated, path, exact}) => {
+  return (<Route  
+    path={path}
+    exact={exact}
+    render={props => 
+      !isAuthenticated ? <Component {...props} /> : <Redirect to='/home' /> }
+  />)
+}
+
+const mapStateToProps = (state) => {
+  return { isAuthenticated: state.session.isAuthenticated };
+};
+
+export const PrivateRoute =  withRouter(connect(mapStateToProps)(Private));
+export const ProtectedRoute =  withRouter(connect(mapStateToProps)(Protected));
