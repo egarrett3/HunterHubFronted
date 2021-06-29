7e96ffc938a44a0beb97afcc908e59a602503fc2
edwardg <egarrett3@fordham.edu>
2021-06-28 21:52:13 -0500

    return json error msg

--- src/reducers/errors_reducer.js
+++ src/reducers/errors_reducer.js
@@ -4,13 +4,14 @@ import merge from 'lodash/merge';
 const _nullErrors = {}
 
 const authErrorReducer = (state = {}, action) => {
+
     switch(action.type) {
         case RECEIVE_AUTH_ERRORS:
             if (action.err) {
                 const errorMsg = action.err
-                const error = merge({},errorMsg)
-                return error
-            }
+                const error = merge({}, state, errorMsg);
+                return { errorMessage: error }
+            } 
         case RECEIVE_CURRENT_USER:
             return _nullErrors;
         case CLEAR_ERRORS:
