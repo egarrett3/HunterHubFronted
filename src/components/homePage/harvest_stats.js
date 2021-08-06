b6b6e571ecf7f316dac7fcf29a73e4c7221b95ab
edwardg <egarrett3@fordham.edu>
2021-08-06 11:32:46 -0600

    cut out unit filter

--- src/components/homePage/harvest_stats.js
+++ src/components/homePage/harvest_stats.js
@@ -1,56 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import isEmpty from "lodash/isEmpty";
-import RenderList from './render_list';
+import StatsUnit from './stats_unit';
 
 const HarvestStats = ({}) => {
 
-  const harvestData = useSelector((state) => {
-    debugger
+  const [harvestData] = useSelector((state) => {
     if (!isEmpty(state.harvestData)) {
       let unitArr = []
-      for (let i = 0; i < state.harvestData.length; i++) {
-        state.harvestData[i].forEach((element) => {
-          if ((element[0] === "Unit" && element[1] === "1") || 
-              (element[0] === "Area" && element[1] === "1-1")) {
-            unitArr.push(state.harvestData[i]);
-          }
-        });
-      }
-      debugger
+      unitArr.push(state.harvestData[0]);
       return unitArr;
     } else {
       return [];
     }
   });
 
-  const formatHarvestData = () => {
-    
-    return (
-      <>
-        {harvestData.map((element) => {
-        
-        return (
-          <>
-            <div className='stats-list'>
-
-              {element.map((ele,idx) => {
-                return (
-                  <div className="stats-unit" key={idx}>
-                    <span>{ele[0]}:&nbsp;</span>
-                    <span>{ele[1]}</span>
-                  </div>
-                );
-              })}
-
-            </div>
-          </>
-        )
-
-      })}
-      </>
-    )
-  }
+  const formatHarvestData = () => (
+    <div className="stats-presentation">
+      <div className="stats-list">
+        {harvestData.map((ele) => {
+          return <StatsUnit key={ele[0]} field={ele[0]} value={ele[1]} />;
+        })}
+      </div>
+    </div>
+  );
 
   useEffect(() => {
     formatHarvestData()
