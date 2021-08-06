9f6c6274bb2d136e4a6b5a080aa65429c8eecdd0
edwardg <egarrett3@fordham.edu>
2021-08-06 11:26:52 -0600

    change fetch dependencies && filter seasonal animalList

--- src/components/homePage/stats_frame.js
+++ src/components/homePage/stats_frame.js
@@ -5,33 +5,54 @@ import StatsOptions from './stats_options';
 import HarvestStats from './harvest_stats'
 import setStatsFilter from '../../actions/harvest_stats_actions';
 
-const StatsFrame = () => {
+const StatsFrame = ({season}) => {
   const animalObj = useSelector((state) => {
     return state.HarvestHeaders
   })
 
   const animals = Object.keys(animalObj);
   const yrs = Object.values(animalObj[animals[0]]);
-  const seasn = Object.keys(animalObj[animals[0]]);
+  let animalList = [];
+
+  if (season === "controlled") {
+    animalList = animals.filter(
+      (animal) => animalObj[animal]["controlled"].length !== 0
+    );
+  } else if (season === "general") {
+    animalList = animals.filter(
+      (animal) => animalObj[animal]["general"].length !== 0
+    );
+  }
+  // const season = Object.keys(animalObj[animals[0]]);
 
   const [years, setYears] = useState(yrs);
-  const [seasons, setSeasons] = useState(seasn);
+  // const [seasons, setSeasons] = useState(season);
 
   const [payload, setPayload] = useState({
     animal: animals[0],
     year: yrs[0][0],
-    season: seasn[0],
+    season: season,
   });
   
   const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   if (season === 'controlled') {
+  //     animalList = animals.filter(animal => animalObj[animal]["controlled"].length !== 0);
+  //   } else if (season === 'general') {
+  //     animalList = animals.filter(animal => animalObj[animal]["general"].length !== 0);
+  //   }
+  // },[season,animalObj])
 
   useEffect(() => {
     dispatch(fetchOptions())
   },[]);
 
   useEffect(() => {
+    debugger
     dispatch(fetchStats(payload));
-  }, [payload]);
+    debugger
+  }, [payload.year,payload.season]);
 
   return (
     <div className="harvest-stats-container">
@@ -39,10 +60,10 @@ const StatsFrame = () => {
         animalObj={animalObj}
         setPayload={setPayload} 
         setYears={setYears}
-        setSeasons={setSeasons}
-        animals={animals} 
+        // setSeasons={setSeasons}
+        animals={animalList} 
         years={years} 
-        seasons={seasons}
+        season={season}
       />
       <HarvestStats />
     </div>
