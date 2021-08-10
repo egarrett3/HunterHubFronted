e18711b6442dfee3e7878215f6befe71fa6a749d
edwardg <egarrett3@fordham.edu>
2021-08-10 11:30:27 -0600

    displaying idaho unit map

--- src/components/homePage/home.js
+++ src/components/homePage/home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import StatsFrame from './stats_frame';
-import Map from './map';
+import MyMap from './map';
 import '../../stylesheet/home_page.css';
 
 const Home = () => {
 
     return (
-        <>
-            <Map />
-            <StatsFrame season={'general'}/>
-        </>
-    )
+      <>
+        <MyMap />
+        <StatsFrame season={"general"} />
+      </>
+    );
 }
 
 export default Home;
--- src/components/homePage/map.js
+++ src/components/homePage/map.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { useDispatch } from 'react-redux';
 import { getUnitMap } from '../../actions/map_actions';
+import { MapContainer, GeoJSON } from "react-leaflet";
+import mapData from '../../assets/data/mapData.json';
 
-const Map = ({}) => {
+const MyMap = ({}) => {
 
     const dispatch = useDispatch();
 
     return (
-        <div className='map-container'> 
-            <div onClick={() => dispatch(getUnitMap())}>RETREIVE MAP DATA</div>
-        </div>
-    )
+      <div className="map-container">
+        <div onClick={() => dispatch(getUnitMap())}>RETREIVE MAP DATA</div>
+          <MapContainer 
+            style={{ height: "80vh", width: "80wh"}}
+            zoom={6}
+            center={[45.5,-114.4]}
+          >
+              <GeoJSON data={mapData.features}/>
+          </MapContainer>
+      </div>
+    );
 }
 
-export default Map;
+export default MyMap;
